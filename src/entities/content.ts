7032f04d777c14748963f51bc44578beb53e832b
jhordan <jhordan.suzuki@lmcorp.local>
2025-05-22 22:05:18 -0300

    feat: add entitie of content

---
+++ src/entities/content.ts
@@ -0,0 +1,17 @@
+export class Content {
+  private readonly content: string;
+
+  get value() {
+    return this.content;
+  }
+
+  private validateContentLength(content: string) {
+    return content.length >= 5 && content.length <= 240;
+  }
+
+  constructor(content: string) {
+    const isValidLength = this.validateContentLength(content);
+    if (isValidLength) throw new Error('Content length is invalid');
+    this.content = content;
+  }
+}
