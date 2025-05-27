d849b37fceec29144f1fa534aec38a134a9773f9
jhordan <jhordan.suzuki@lmcorp.local>
2025-05-27 17:11:22 -0300

    fix: rename controller

--- src/http/modules/http.module.ts
+++ src/http/modules/http.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AlertsController } from 'src/http/controller/app.controller';
+import { AlertsController } from 'src/http/controller/alert.controller';
 import { DatabaseModule } from 'src/service/modules/database.module';
 import { ExecuteAlert } from 'src/use-cases/execute-alert';
 
